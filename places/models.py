61d311067ca2cef29a0dd1b64c3ae7de5dcc074e
Александр Lead <leont_aa@mail.ru>
2025-10-23 21:13:41 +0800

    Add frontend (updated)

--- places/models.py
+++ places/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
+class Place(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Название")
+    de
+
+    def __str__(self):
+        return self.name
+
 # Create your models here.
